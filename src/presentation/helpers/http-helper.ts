cf3e984a1b2c6a23f3be0753b80838f40f9c58f3
RobertOcsV <robertoliveiracor@hotmail.com>
2023-08-02 06:33:04 -0300

    refactor: change serverOk object name account to data, making the method more generic

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -11,7 +11,7 @@ export const serverError = (): HttpResponse => ({
   body: new ServerError()
 })
 
-export const serverOk = (account: any): HttpResponse => ({
+export const serverOk = (data: any): HttpResponse => ({
   statusCode: 200,
-  body: account
+  body: data
 })
