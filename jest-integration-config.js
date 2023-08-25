47305d1ea580d54070938e9529841ebdee7b970f
RobertOcsV <robertoliveiracor@hotmail.com>
2023-08-25 00:06:28 -0300

    feat: ensure requests parse body as json

--- jest-integration-config.js
+++ jest-integration-config.js
@@ -1,4 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const config = require('./jest.config')
-config.testMatch = ['**/*.test.js']
+config.testMatch = ['**/*.test.ts']
 module.exports = config
